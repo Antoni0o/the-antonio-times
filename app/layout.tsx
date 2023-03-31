813149bc0ceabee94ef0d69873978d60df7579e7
Antonio <antonio.dias8997@gmail.com>
2023-03-31 07:33:39 -0300

    Add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import { ReactNode } from "react";
 import Navbar from "../components/Navbar";
 import "../public/styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 interface ILayout {
   children: ReactNode;
@@ -9,18 +10,21 @@ interface ILayout {
 
 export default function Layout({ children }: ILayout) {
   return (
-    <html>
-      <head>
-        <title>The Antonio Times</title>
-      </head>
-      <body className="dark:bg-slate-500">
-        <nav className="flex items-center justify-center m-3 p-3 border-slate-200 rounded bg-slate-100 dark:bg-slate-800 dark:border-slate-700 shadow-lg">
-          <Navbar />
-        </nav>
-        <main className="m-3 rounded bg-slate-100 dark:bg-slate-800 dark:text-white shadow-xl">
-          {children}
-        </main>
-      </body>
-    </html>
+    <>
+      <html>
+        <head>
+          <title>The Antonio Times</title>
+        </head>
+        <body className="dark:bg-slate-500">
+          <nav className="flex items-center justify-center m-3 p-3 border-slate-200 rounded bg-slate-100 dark:bg-slate-800 dark:border-slate-700 shadow-lg">
+            <Navbar />
+          </nav>
+          <main className="m-3 rounded bg-slate-100 dark:bg-slate-800 dark:text-white shadow-xl">
+            {children}
+          </main>
+        </body>
+      </html>
+      <Analytics />
+    </>
   );
 }
